01a9c0ae6ae0d8476f80c5ee46404fdee6f56424
José Vitor <jvocse@gmail.com>
2022-10-27 16:49:40 -0400

    Criando servidor do chat e mandando estatus de conectado

---
+++ index.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+
+    console.log('Usuário conectado!');
+    //io.emit('conectado','Estou conectado!');
+    socket.broadcast.emit('novo usuario','Um novo usuário se conectou!');
+
+    socket.on('disconnect',()=>{
+        console.log('Desconectado.');
+    })
+
+});
+
+http.listen(3000, () => {
+
+    console.log('listening on *:3000');
+
+});
